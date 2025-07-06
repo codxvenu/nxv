db378a0b487f0aebcb4001c59563fa067fa164f8
codxvenu <158077441+codxvenu@users.noreply.github.com>
2025-07-06 12:30:01 +0530

    Update activate-free-trial.js

--- pages/api/activate-free-trial.js
+++ pages/api/activate-free-trial.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
     }
 
     // Activate free trial using the upgrade logic
-    const upgradeResult = await upgradeUserPlan(user.id, 'Free Trial');
+    const upgradeResult = await upgradeUserPlan(user.id, 'Premium');
 
     res.status(200).json({
       success: true,
