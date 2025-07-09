e45f386f13a99a8900b391373d9510f49cf8f1b4
codxvenu <158077441+codxvenu@users.noreply.github.com>
2025-07-09 10:59:16 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import "@fontsource/orbitron";
 import "@fontsource/roboto-mono";
 import { AuthProvider } from "@/lib/authContext";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Analytics/>
     </AuthProvider>
   );
 }
